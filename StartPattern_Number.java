11ea26f963ba872c645e6daa80cdd1a088d1018d
amitsinha9598 <103451609+amitsinha9598@users.noreply.github.com>
2023-02-25 15:08:03 +0530

    Add files via upload

---
+++ StartPattern_Number.java
@@ -0,0 +1,50 @@
+public class StartPattern_Number {
+
+	public static void main(String[] args) {
+		System.out.println("Ex2 .........\n"); 
+		for(int i*;i <=*;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" "+j);
+				
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Ex3 .........\n"); 
+		for(int i=1;i <=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" "+i);
+				
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Ex4 .........\n"); 
+		for(int i = 65;i <=70;i++) {
+			for(int j=65;j<=i;j++) {
+				System.out.print(" "+(char)j);
+				
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Ex5 .........\n"); 
+		for(char i = 'A';i <='F';i++) {
+			for(char j='A';j<=i;j++) {
+				System.out.print(" "+j);
+				
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Ex6 .........\n"); 
+		for(char i = 'A';i <='F';i++) {
+			for(char j='A';j<=i;j++) {
+				System.out.print(" "+(int)j);
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
